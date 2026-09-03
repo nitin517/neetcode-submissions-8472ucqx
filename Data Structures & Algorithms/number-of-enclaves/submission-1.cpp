967220ef745cf8d1a5b06fafa156cb3500db33d5
Nitin Chaurasiya <nitinchaurasiya1749@gmail.com>
2026-09-03 19:28:58 +0530

    Add: number-of-enclaves - submission-1

---
+++ Data Structures & Algorithms/number-of-enclaves/submission-1.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+
+    int numEnclaves(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(grid[i][j] == 1 && vis[i][j] == 0) {
+
+                    int cnt = 0;
+
+                    bool boundary = (i == 0 || i == n-1 ||
+                                     j == 0 || j == m-1);
+
+                    queue<pair<int,int>> q;
+
+                    q.push({i, j});
+                    vis[i][j] = 1;
+
+                    while(!q.empty()) {
+
+                        int x = q.front().first;
+                        int y = q.front().second;
+                        q.pop();
+
+                        cnt++;
+
+                        if(x == 0 || x == n-1 ||
+                           y == 0 || y == m-1) {
+                            boundary = true;
+                        }
+
+                        for(int k = 0; k < 4; k++) {
+
+                            int nx = x + dx[k];
+                            int ny = y + dy[k];
+
+                            if(nx >= 0 && nx < n &&
+                               ny >= 0 && ny < m) {
+
+                                if(grid[nx][ny] == 1 &&
+                                   vis[nx][ny] == 0) {
+
+                                    vis[nx][ny] = 1;
+                                    q.push({nx, ny});
+                                }
+                            }
+                        }
+                    }
+
+                    if(!boundary)
+                        ans += cnt;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
